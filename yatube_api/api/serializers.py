f134c141c920ddca1e6440c3f2a2317ed7b91fb3
Владислав Елькин <vladel_2003@bk.ru>
2025-01-29 20:59:06 +0700

    Read-only fix

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -22,6 +22,7 @@ class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         slug_field='username', read_only=True
     )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)  # Делаем `post` только для чтения
 
     class Meta:
         model = Comment
